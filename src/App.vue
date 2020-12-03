8f00c198e5a47636b46c06c8da7cbb2aaa2fe143
hyesech <hyesech@gmail.com>
2020-12-03 20:08:41 +0900

    Clear All: 스크린과 로컬스토리지 데이터 연동

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <TodoHeader></TodoHeader>
     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
     <TodoList v-bind:propsdata="todoItems" v-on:removeTodoItem="removeOneItem"></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -29,7 +29,11 @@ export default {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
       console.log(todoItem);
-    }            
+    },
+    clearAllItems: function(){
+      localStorage.clear();
+      this.todoItems = [];
+    }      
   },
   created: function(){
     if(localStorage.length > 0){
