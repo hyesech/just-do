9394cde38d559fca2c9a24ea7523ef9beed02376
hyesech <hyesech@gmail.com>
2020-12-18 15:15:52 +0900

    created, todoItem: store state로 추가

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,23 @@ import Vuex from "vuex";
 // Vue 전역에, 그러니까 global functionallity를 사용하고 싶을 때 이것을 씁니다.
 Vue.use(Vuex);
 
+const storage = {
+  // 속성 method임 로컬스토리지에 저장된 데이터를 가져와서 배열로 돌려주는 것임.
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  }
+};
+
 export const store = new Vuex.Store({
-  state: {}
+  state: {
+    todoItems: storage.fetch()
+  }
 });
